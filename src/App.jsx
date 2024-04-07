56523d3abd6ba129d019b9a991c88441c6b74580
spaceTony9 <bilokon.fw7@gmail.com>
2024-04-07 21:34:50 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -62,7 +62,6 @@ export default function App() {
     const [selectedPhoto] = photos.filter(photo => photo.id === id);
     setModalContent(selectedPhoto);
   }
-
   return (
     <>
       <SearchBar onSubmit={onFormSubmit} />
